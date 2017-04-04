6eca1e7b795d02bc04456823bc1f116d748e05ac
XeryusTC <armadillo@onenetbeyond.org>
2017-04-04 11:22:11 +0200

    Shamelessly steal Material colors

--- simulation/visualisation.py
+++ simulation/visualisation.py
@@ -4,7 +4,10 @@ import numpy as np
 from PIL import Image, ImageDraw
 
 class Visualisation():
-    colors = ('red', 'green', 'blue', 'magenta', 'orange', 'purlple')
+    colors = ('#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3',
+              '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39',
+              '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#9e9e9e',
+              '#607d8b')
     def __init__(self, world, scale=10):
         self.scale = scale
         self.world = world

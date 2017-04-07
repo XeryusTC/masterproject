574601954e3b786299bc4a298d3143bb837e11f5
XeryusTC <armadillo@onenetbeyond.org>
2017-04-07 20:49:24 +0200

    Make RRA* work like A*, not BFS

--- rra_star.py
+++ rra_star.py
@@ -5,6 +5,9 @@ import timeit
 
 import world
 
+class NoValidPathExists(Exception):
+    pass
+
 class RRAstar:
     def __init__(self, world, origin, goal):
         self.world = world
@@ -20,7 +23,7 @@ class RRAstar:
             return self.g[node]
         if self._resume(node):
             return self.g[node]
-        return None
+        raise NoValidPathExists()
 
     def _resume(self, node):
         while self.open:
@@ -37,14 +40,14 @@ class RRAstar:
                 cost = self.g[current] + 1
                 if neighbour not in self.g:
                     heapq.heappush(self.open,
-                        (cost + self._h(neighbour), neighbour))
+                        (cost + self._h(neighbour, node), neighbour))
                 elif cost > self.g[neighbour]:
                     continue # This path is longer
                 self.g[neighbour] = cost
         return False
 
-    def _h(self, pos):
-        return max(abs(pos[0] - self.goal[0]), abs(pos[1] - self.goal[1]))
+    def _h(self, cur, node):
+        return max(abs(cur[0] - node[0]), abs(cur[1] - node[1]))
 
 
 if __name__ == '__main__':
@@ -53,9 +56,26 @@ if __name__ == '__main__':
     origins = tuple(random.choice(w.passable) for i in range(tests))
     goals = tuple(random.choice(w.passable) for i in range(tests))
 
+    print('Starting')
     start = timeit.default_timer()
     for i in range(tests):
         heuristic = RRAstar(w, origins[i], goals[i])
     end = timeit.default_timer()
-    print('elapsed time:', end - start)
-    print('mean time:', (end - start) / tests)
+    print('elapsed time:', (end - start) * 1000)
+    print('mean time:', (end - start) / tests * 1000)
+
+    # Export heuristic costs
+    from PIL import Image, ImageDraw, ImageFont
+    scale = 20
+    im = w.as_image(scale=scale)
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.load_default()
+    draw.rectangle((goals[-1][0]*scale, goals[-1][1]*scale,
+        (goals[-1][0]+1)*scale, (goals[-1][1]+1)*scale), fill='green')
+    draw.rectangle((origins[-1][0]*scale, origins[-1][1]*scale,
+        (origins[-1][0]+1)*scale, (origins[-1][1]+1)*scale), fill='red')
+    for pos in heuristic.closed:
+        cost = heuristic.g[pos]
+        #cost = heuristic._h(pos)
+        draw.text((pos[0]*scale+2, pos[1]*scale+2), str(cost), "black", font)
+    im.save('rra_star.png')

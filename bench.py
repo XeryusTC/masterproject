bbb8b8178bf1d9937160aca728f7753ac3080d41
XeryusTC <armadillo@onenetbeyond.org>
2017-06-19 12:46:53 +0200

    Add version1 to benchmarks

--- bench.py
+++ bench.py
@@ -7,6 +7,7 @@ import rra_star
 from simulation import util, visualisation
 import id as odid
 import poc
+import version1
 
 MAX_TIME = 2
 
@@ -21,7 +22,7 @@ def main(runs, max_agents):
 
     f = open('poc_bench.csv', 'w')
     i = 0
-    f.write('num,od,poc\n')
+    f.write('num,od,poc,version1\n')
 
     global_start_time = timeit.default_timer()
     for problem in problems:
@@ -46,21 +47,38 @@ def main(runs, max_agents):
         # Run proof of concept
         start_time = timeit.default_timer()
         try:
-            start_time = timeit.default_timer()
             agents = [poc.Agent(problem[0], problem[1][i], problem[2][i])
                 for i in range(len(problem[1]))]
             poc.poc(agents, start_time=start_time, max_time=MAX_TIME)
             end_time = timeit.default_timer()
-            f.write(f'{end_time-start_time}\n')
+            f.write(f'{end_time-start_time},')
         except odid.TimeExceeded:
             end_time = timeit.default_timer()
             print('POC failed')
+            f.write('NA,')
+        except (rra_star.NoValidPathExists, util.NoPathsFoundException):
+            print('No valid path exists')
+            f.write('NA,')
+        finally:
+            print(f'POC time: {(end_time - start_time) * 1000:5.3f}ms')
+
+        # Run Version 1
+        start_time = timeit.default_timer()
+        try:
+            agents = [version1.Agent(problem[0], problem[1][i], problem[2][i])
+                for i in range(len(problem[1]))]
+            version1.version1(agents, start_time, MAX_TIME, False)
+            end_time = timeit.default_timer()
+            f.write(f'{end_time-start_time}\n')
+        except odid.TimeExceeded:
+            end_time = timeit.default_timer()
+            print('Version1 failed')
             f.write('NA\n')
         except (rra_star.NoValidPathExists, util.NoPathsFoundException):
             print('No valid path exists')
             f.write('NA\n')
         finally:
-            print(f'POC time: {(end_time - start_time) * 1000:5.3f}ms')
+            print(f'Version1 time: {(end_time - start_time) * 1000:5.3f}ms')
 
         # Write latest results to file
         f.flush()
--- bench_graph.R
+++ bench_graph.R
@@ -2,6 +2,15 @@ dat = read.csv('poc_bench.csv', header=T)
 len = length(dat$num)
 od = sort(dat$od)
 poc = sort(dat$poc)
-plot(seq(length(od)), od, type='l', log='y', col='red', xlim=c(0,len), ylim=c(1e-3,2), xlab='Instance', ylab='Time (s)')
+version1 = sort(dat$version1)
+plot(seq(length(od)), od,
+     type='l',
+     log='y',
+     col='red',
+     xlim=c(0,len),
+     ylim=c(1e-3,2),
+     xlab='Instance',
+     ylab='Time (s)')
 lines(seq(length(poc)), poc, col='blue')
+lines(seq(length(version1)), version1, col='green')
 abline(h=c(0.01, 0.1, 1,2), untf=T, col='lightgrey')

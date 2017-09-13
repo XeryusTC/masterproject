1bcf1a38ad8f9c4f90c076fcb01a04041f3bf91e
XeryusTC <armadillo@onenetbeyond.org>
2017-09-13 14:32:12 +0200

    Add windowed version to benchmark, also calculate path lengths

--- experiments/benchmark.py
+++ experiments/benchmark.py
@@ -12,6 +12,7 @@ import poc
 import version1
 import version1b
 import standard_algorithm
+import window
 
 MAX_TIME = 2
 OBSTACLES = 0.2
@@ -39,12 +40,21 @@ def standard_algorithm_entry(world, starts, goals, start_time, max_time):
     return standard_algorithm.standard_algorithm(len(starts), world, starts,
                                                  goals, start_time, max_time)
 
-Algorithm = namedtuple('Algorithm', ['name', 'entry'])
+def window_entry(world, starts, goals, start_time, max_time, window_size):
+    agents = [window.Agent(world, starts[i], goals[i], window_size)
+              for i in range(len(starts))]
+    return window.window_version(agents, window_size, start_time, max_time,
+                                 False)
+
+Algorithm = namedtuple('Algorithm', ['name', 'entry', 'kwargs'])
 ALGORITHMS = [
-    Algorithm('OD+ID', odid_entry),
-    Algorithm('Naive', poc_entry),
-    Algorithm('Base version', version1_entry),
-    Algorithm('Version 1b', version1b_entry),
+    Algorithm('OD+ID', odid_entry, {}),
+    Algorithm('Naive', poc_entry, {}),
+    Algorithm('Base version', version1_entry, {}),
+    Algorithm('Version 1b', version1b_entry, {}),
+    Algorithm('Window 8', window_entry, {'window_size': 8}),
+    Algorithm('Window 4', window_entry, {'window_size': 4}),
+    Algorithm('Window 2', window_entry, {'window_size': 2}),
 ]
 
 def main(runs, max_agents):
@@ -60,29 +70,42 @@ def main(runs, max_agents):
     line = []
     f = open(f'results/benchmark-{datetime.now()}.csv', 'w')
     writer = csv.writer(f)
-    result = ['instance'] + [algorithm.name for algorithm in ALGORITHMS]
+    result = ['instance', 'optimal length']
+    for algorithm in ALGORITHMS:
+        result += [algorithm.name, f'{algorithm.name}_length']
     writer.writerow(result)
 
     global_start_time = timeit.default_timer()
     for problem in problems:
-        result = [instance]
+        print('Determining optimal lengths')
+        optimal_length = 0
+        try:
+            for i in range(len(problem[1])):
+                dist = rra_star.RRAstar(problem[0], problem[1][i],
+                                        problem[2][i])
+                optimal_length += dist.dist(problem[1][i])
+        except rra_star.NoValidPathExists:
+            optimal_length = 'NA'
+
+        result = [instance, optimal_length]
         for algorithm in ALGORITHMS:
             start_time = timeit.default_timer()
             try:
-                algorithm.entry(*problem,
-                                start_time=start_time,
-                                max_time=MAX_TIME)
+                paths = algorithm.entry(*problem,
+                                        start_time=start_time,
+                                        max_time=MAX_TIME, **algorithm.kwargs)
+                length = sum(len(p) for p in paths)
                 end_time = timeit.default_timer()
-                result.append(end_time - start_time)
+                result += [end_time - start_time, length]
             except odid.TimeExceeded:
                 print('Time exceeded')
-                result.append('NA')
+                result += ['NA', 'NA']
             except (rra_star.NoValidPathExists, util.NoPathsFoundException):
                 print('No valid path exists')
-                result.append('NA')
+                result += ['NA', 'NA']
             except version1b.ConflictNotSolved:
                 print('Could not find a solution to a conflict')
-                result.append('NA')
+                result += ['NA', 'NA']
             finally:
                 end_time = timeit.default_timer()
                 print(algorithm.name, 'time:',

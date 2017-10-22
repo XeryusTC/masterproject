cb33afa7bfac93f2f0499f9abf05f4a1ea48b101
XeryusTC <armadillo@onenetbeyond.org>
2017-10-22 16:47:54 +0200

    Ignore knitr output, write script to remove trailing whitespace

---
+++ remove-trailing-whitespace.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sed -i 's/[ \t]*$//' reports/final/main.Rnw
+sed -i 's/[ \t]*$//' reports/final/tex/results.Rnw
+sed -i 's/[ \t]*$//' reports/final/tex/abstract.tex
+sed -i 's/[ \t]*$//' reports/final/tex/discussion.tex
+sed -i 's/[ \t]*$//' reports/final/tex/intro.tex
+sed -i 's/[ \t]*$//' reports/final/tex/problem.tex
+sed -i 's/[ \t]*$//' reports/final/tex/related_work.tex
